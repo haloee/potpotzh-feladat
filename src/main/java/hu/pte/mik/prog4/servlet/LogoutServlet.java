c82b9740335d3e6db32bda41a61b1aad36755ae4
Sztaracsek Matyas <sztaracsekmatyas@gmail.com>
2025-05-20 15:54:05 +0200

    login, loginfailed es logoutservlet

---
+++ src/main/java/hu/pte/mik/prog4/servlet/LogoutServlet.java
@@ -0,0 +1,18 @@
+package hu.pte.mik.prog4.servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
+        req.logout();
+        resp.sendRedirect("login.html");
+    }
+}
